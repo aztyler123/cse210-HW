6e6333e591eb3d5e1dea415c8e2a8b24308fd87f
Tyler H <aztyler2002@gmail.com>
2026-04-22 14:51:34 -0700

    Assignmnet completed for Prep #1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        //Full separate variable walk through
+        int x;
+        x = 5;
+        Console.WriteLine(x);
+        // All in one go
+        int y=5;
+        Console.WriteLine(y);
+        //How to ask for a user input in C# also assignment
+        Console.Write("Enter your First Name: ");
+        string firstname = Console.ReadLine();
+        Console.Write("Enter your Last Name: ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"My name is {lastname}, {firstname} {lastname}");
+
+
     }
+
 }
