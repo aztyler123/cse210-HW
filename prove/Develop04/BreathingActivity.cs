1d657a0427baa0d74558639a890ea17d9cedc764
Tyler H <aztyler2002@gmail.com>
2026-06-20 15:43:05 -0700

    all good

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -5,30 +5,28 @@ namespace MindfulnessApp
     //our two  public calls
     public class BreathingActivity : Activity
     {
-        public class BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.")
+        public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.")
+        {}
+        public override void RunActivity()
         {
-        }
-        
-            public override void RunActivity()
+            DisplayStartingMessage();
+
+            int elapsed = 0;
+            while (elapsed < _duration)
             {
-                DisplayStartingMessage();
+                Console.WriteLine("\nBreathe in...");
+                ShowCountdown(5);
+                elapsed += 5;
 
-                int elapsed = 0;
-                while (elapsed < _duratiion)
-                {
-                    Console.WriteLine("\nBreathe in...");
-                    ShowCountdown(5);
-                    elapsed += 5;
+                if (elapsed >= _duration) break;
 
-                    if (elapsed >= _duration) break;
+                Console.WriteLine("Breathe out...");
+                ShowCountdown(5);
+                elapsed += 5;
+            }
 
-                    Console.WriteLine("Breathe out...");
-                    ShowCountdown(5);
-                    elapsed += 5;
-                }
-
-                DisplayEndingMessage();
-                //I think I got off deleting some notes and my tabs are off
+            DisplayEndingMessage();
+            //I think I got off deleting some notes and my tabs are off
         }
     }
 }
