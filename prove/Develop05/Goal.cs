9f1be39173f4fe163b1cb6ec937c243c136f669b
Tyler H <aztyler2002@gmail.com>
2026-07-12 13:59:59 -0700

    Got Goals and SimpleGoals started up

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace EternalQuest
+{
+    public abstract class Goal //this will contain privates that are encapsalted.
+    {
+        private string _shortName;
+        private string _description;
+        private int _points;
+
+        public Goal(string name, string description, int points)
+        {
+            _shortName = name;
+            _description = description;
+            _points = points;
+
+        //made variables that are easily accessible
+        }
+
+        //Encapulation
+        public string ShortName => _shortName; //separator from member name and implementation makes it a "get"
+        public string Description => _description;
+        public int Points => _points;
+
+
+        //polymorphic things
+        public abstract int RecordEvent();
+        public abstract bool IsComplete();
+        public abstract string GetDetailsString();
+        public abstract string GetStringRepresent();
+    }
+}
---
+++ prove/Develop05/SimpleGoals.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace EternalQuest
+{
+    public class SimpleGoal : Goal
+    {
+        private bool _isComplete;
+
+        public SimpleGoal(string name, string description, int points, bool isComplete = false) : base(name, description, points)
+        {
+            _isComplete = isComplete;
+        }
+
+        public override int RecordEvent()
+        {
+            if (!_isComplete)
+            {
+                _isComplete = true;
+                return Points;
+            }
+            return 0;
+        }
+        public override bool IsComplete() => _isComplete;
+        
+        public override string GetDetailsString()
+        {
+            string statusSymbol = _isComplete ? "[X]" : "[ ]";
+            return $"{statusSymbol} {ShortName} ({Description})";
+        }
+        public override string GetStringRepresent()
+        {
+            return $"SimpleGoal:{ShortName}, {Description}, {Points}, {_isComplete}";
+        }
+    }
+    
+}
