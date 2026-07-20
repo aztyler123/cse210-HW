25b81334fb019034a9fb1c64ad509dca792d5aea
Tyler H <aztyler2002@gmail.com>
2026-07-20 08:09:12 -0700

    Swimming and cycling finished according to diagram and idea or necessary calculations

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -3,5 +3,21 @@ using System.Collections.Generic;
 
 namespace FitnessTracker
 {
-    
+    public class Cycling : Activity
+    {
+        private double _speed;
+
+        public Cycling(string date, int minutes, double speed) : base(date, minutes)
+        {
+            _speed = speed;
+        }
+        public override double GetSpeed()
+        {
+            return _speed;
+        }
+        public override double GetDistance()
+        {
+            returnn(_speed * Minutes) / 60;
+        }
+    }
 }
--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -1,7 +1,21 @@
 using System;
 using System.Collections.Generic;
-
+//follow tje written plan for each activity pretty simple.
 namespace FitnessTracker
 {
-    
+    public class Swimming : Activity
+    {
+        private int _laps;
+
+        public Swimming(string date, int minutes, int laps) : base(date, minutes)
+        {
+            _laps = laps;
+        }
+
+        public override double GetDistance()
+        {
+            //conversion equation provided.
+            return _laps * 50.0 / 1000.0 * 0.62;
+        }
+    }
 }
